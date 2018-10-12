9cee7d954c26c3c78b7f0c71bac6e6a4f2ac60fb
SmallAcmen <43090622+SmallAcmen@users.noreply.github.com>
2018-10-12 23:40:22 +0800

    新建项目:自动标注

---
+++ AutoCADProject/AutoDimensions/RotateDirection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDimensions
+{
+    /// <summary>
+    /// 旋转方向
+    /// </summary>
+    public enum RotateDirection
+    {
+        /// <summary>
+        /// 东
+        /// </summary>
+        East,
+        /// <summary>
+        /// 南
+        /// </summary>
+        South,
+        /// <summary>
+        /// 西
+        /// </summary>
+        West,
+        /// <summary>
+        /// 北
+        /// </summary>
+        North,
+    }
+}
---
+++ AutoCADProject/AutoDimensions/TopCall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDimensions
+{
+    /// <summary>
+    /// AutoCAD上层调用-
+    /// 提供给UI层调用接口-
+    /// 创建日期2018.10.12
+    /// </summary>
+    class TopCall
+    {
+    }
+}
