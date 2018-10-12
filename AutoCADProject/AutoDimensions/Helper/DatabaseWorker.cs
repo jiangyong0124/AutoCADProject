cefdebaeaed2fd1658f30ab7ceed8d5716f0b8d3
SmallAcmen <43090622+SmallAcmen@users.noreply.github.com>
2018-10-13 00:12:52 +0800

    添加帮助类

---
+++ AutoCADProject/AutoDimensions/Helper/DatabaseWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDimensions.Helper
+{
+    /// <summary>
+    /// 数据库封装-
+    /// 针对web提供对数据库的几种访问-
+    /// 2018.10.12
+    /// </summary>
+    public class DatabaseWorker
+    {
+
+    }
+}
